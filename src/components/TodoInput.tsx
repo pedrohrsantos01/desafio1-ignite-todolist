dd1f09916a4cfa5f6818a52fdf4a4d62cf79d045
Pedro Santos <pedrohrsantos01@gmail.com>
2021-12-29 09:04:31 -0300

    Implementando o TodoInput

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,3 +1,17 @@
+/* 
+Esse é o componente de input onde serão digitados os nomes das tarefas que serão adicionadas. Ao lado do input, existe um botão que adiciona o conteúdo digitado no input na lista de tarefas.
+
+Nesse arquivo temos o estado `task` que deverá ser usado na propriedade `value` do componente `TextInput` 
+e a função `setTask` que deve ser usada na propriedade `onChangeText` também do `TextInput`.
+
+Você deve implementar a função que existe no arquivo que é:
+
+- **handleAddNewTask**: Essa função deve ser chamada quando o botão `TouchableOpacity` for pressionado ou quando a tecla `enter` do teclado for pressionada (use a propriedade `onSubmitEditing` do `TextInput` para isso).
+Ao receber o nome da tarefa na função, é importante que você verifique se esse nome é uma `string` válida. Isto é, o valor recebido **deve** ser diferente de uma `string` vazia.
+Essa função deve chamar a função `addTask` (recebida nas propriedades do componente) passando o estado `task` como argumento. Lembre de limpar o estado `task` sempre que uma nova `task` for adicionada.
+
+*/
+
 import React, { useState } from 'react';
 import { StyleSheet, TextInput, TouchableOpacity, View } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
@@ -7,10 +21,17 @@ interface TodoInputProps {
 }
 
 export function TodoInput({ addTask }: TodoInputProps) {
-  // const [task, setTask] = useState('');
+  const [task, setTask] = useState('');
 
   function handleAddNewTask() {
     //TODO - Call addTask if task not empty and clean input value 
+    if(task !== '') {
+      addTask(task)
+      setTask('')
+    }
+
+    return 
+
   }
 
   return (
@@ -21,12 +42,17 @@ export function TodoInput({ addTask }: TodoInputProps) {
         placeholderTextColor="#B2B2B2"
         returnKeyType="send"
         selectionColor="#666666"
+        value={task}
+        onChangeText={setTask}
+        onSubmitEditing={handleAddNewTask}
+    
         //TODO - use value, onChangeText and onSubmitEditing props
       />
       <TouchableOpacity
         testID="add-new-task-button"
         activeOpacity={0.7}
         style={styles.addButton}
+        onPress={handleAddNewTask}
         //TODO - onPress prop
       >
         <Icon name="chevron-right" size={24} color="#B2B2B2" />
